c8536df8517ff81deb292fae5b56d40212ac5219
Rafael Grimmler da Rocha <grimmlerr@gmail.com>
2021-10-03 01:13:35 -0300

    topbar

--- src/Components/Lobby/Setgamebar/index.js
+++ src/Components/Lobby/Setgamebar/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Container, Hidepart, Contentcontainer, Leftbackground, Rightbackground, Centralbackground, Contentcenter, Pagename } from './styles';
+import { Container, Hidepart, Contentcontainer, Leftbackground, Rightbackground, Centralbackground, Contentcenter, Pagename, Akxcontainer, Lobbyoptionscontainer, Createlobbybutton, Joinlobbybutton, Rankedqualify, Currentgamescontainer, Gamesavailable, Gameslive, Gamesvetoing, Litemode, Litemodecheck, Litemodecheckcontainer, Litemodeinput } from './styles';
 
 function Setgamebar() {
   return (
@@ -14,11 +14,45 @@ function Setgamebar() {
 
             <Centralbackground>
               <Contentcenter>
-
                 <Pagename>
                   <h1>LOBBY</h1>
                 </Pagename>
-
+                <Akxcontainer />
+                <Lobbyoptionscontainer>
+                  <Createlobbybutton>
+                    <div />
+                    <span>CRIAR LOBBY</span>
+                  </Createlobbybutton>
+                  <Joinlobbybutton>
+                    <div />
+                    <span>ENTRAR EM UMA LOBBY</span>
+                  </Joinlobbybutton>
+                  <Rankedqualify>
+                    <div />
+                    <span>RANKED QUALIFY</span>
+                  </Rankedqualify>
+                </Lobbyoptionscontainer>
+                <Currentgamescontainer>
+                  <Gamesavailable>
+                    <span>DISPONÍVEIS</span>
+                    <span>5/10</span>
+                  </Gamesavailable>
+                  <Gameslive>
+                    <span>LIVE</span>
+                    <span>20</span>
+                  </Gameslive>
+                  <Gamesvetoing>
+                    <span>VETANDO</span>
+                    <span>2</span>
+                  </Gamesvetoing>
+                </Currentgamescontainer>
+                <Litemode>
+                  <span>Lite Mode</span>
+                  <Litemodecheckcontainer>
+                    <Litemodecheck />
+                    <Litemodeinput type="checkbox"/>
+                  </Litemodecheckcontainer>
+                </Litemode>
               </Contentcenter>
             </Centralbackground>
         </Contentcontainer>
--- src/Components/Lobby/Setgamebar/styles.js
+++ src/Components/Lobby/Setgamebar/styles.js
@@ -1,7 +1,11 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import TopBackground from '../../../img/Lobby/TopBackground.jpg'
+import TopAKX from '../../../img/Lobby/TopAKX.svg'
+import Topcreatelobbybutton from '../../../img/Lobby/Topcreatelobbybutton.svg'
+import Topjoinlobbybutton from '../../../img/Lobby/Topjoinlobbybutton.svg'
+import Toprankedqualify from '../../../img/Lobby/Toprankedqualify.svg'
 
-export const Container = styled.div`
+export const Container = styled.section`
     width: calc(100% - 80px);
     height: 150px;
     border-bottom: 1px solid var(--header-border-color);
@@ -39,7 +43,7 @@ export const Centralbackground = styled.div`
     position: absolute;
     width: 100%;
     height: 100%;
-    background-color: rgba(28, 29, 44, .88);
+    background-color: rgba(28, 29, 44, .85);
     display: flex;
     justify-content: center;
 `;
@@ -48,6 +52,8 @@ export const Contentcenter = styled.div`
     width: 1200px;
     height: 100%;
     display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
 export const Pagename = styled.div`
@@ -59,8 +65,266 @@ export const Pagename = styled.div`
 
     & h1 {
         color: white;
+        font-size: 29px;
     }
 
+`;
+
+const Akxanimation = keyframes`
+    0% {
+        transform: rotateY(0deg);
+    }
+    30% {
+        transform: rotateY(0deg);
+    }
+    40% {
+        transform: rotateY(180deg);
+    }
+    50%{
+        transform: rotateY(0deg);
+    }
+    100% {
+        transform: rotateY(0deg);
+    }
+`;
+
+export const Akxcontainer = styled.div`
+    width: 40px;
+    height: 35px;
+    margin-left: 13px;
+    background-image: url(${TopAKX});
+    background-size: 40px;
+    background-position: center center;
+    background-repeat: no-repeat;
+    filter: saturate(0);
+    animation: ${Akxanimation} 4s linear infinite ;
+`;
+
+export const Lobbyoptionscontainer = styled.div`
+    width: 513px;
+    height: 40px;
+    margin-left: 15px;
+    display: flex;
+`;
+
+export const Createlobbybutton = styled.button`
+    width: 137px;
+    height: 100%;
+    background-color: #a4bf19;
+    border: 1px solid #a4bf19;
+    border-radius: 2px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    transition: background-color 0.5s;
+
+    & div {
+        width: 30px;
+        height: 25px;
+        background-image: url(${Topcreatelobbybutton});
+        background-size: 17px;
+        background-repeat: no-repeat;
+        background-position: center center;
+    }
+
+    & span {
+        color: white;
+        font-stretch: extra-condensed;
+        font-weight: bold;
+        font-size: 15px;
+    }
+
+    &:hover {
+        background-color: #7D9503;
+    }
+`;
+
+export const Joinlobbybutton = styled.div`
+    width: 196px;
+    height: 100%;
+    background-color: #496cd6;
+    border: 1px solid #6887e3;
+    border-radius: 2px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    margin-left: 10px;
+    transition: background-color 0.5s;
+    padding-right: 5px;
+
+    & div {
+        width: 30px;
+        height: 25px;
+        background-image: url(${Topjoinlobbybutton});
+        background-size: 17px;
+        background-repeat: no-repeat;
+        background-position: center center;
+    }
+
+    & span {
+        color: white;
+        font-stretch: extra-condensed;
+        font-weight: bold;
+        font-size: 15px;
+    }
+
+    &:hover {
+        background-color: #4040E7;
+    }
+`;
+
+export const Rankedqualify = styled.div`
+    width: 162px;
+    height: 100%;
+    background-color: #bd6531;
+    border: 1px solid #bf7143;
+    border-radius: 2px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    margin-left: 10px;
+    transition: background-color 0.5s;
+    padding-right: 5px;
+
+    & div {
+        width: 30px;
+        height: 25px;
+        background-image: url(${Toprankedqualify});
+        background-size: 25px;
+        background-repeat: no-repeat;
+        background-position: center center;
+        margin-right: 3px;
+    }
+
+    & span {
+        color: white;
+        font-stretch: extra-condensed;
+        font-weight: bold;
+        font-size: 15px;
+    }
+
+    &:hover {
+        background-color: #a64a14;
+    }
+`;
+
+export const Currentgamescontainer = styled.div`
+    width: 339px;
+    height: 20px;
+    margin-left: 15px;
+    display: flex;
+
+    & span {
+        color: white;
+        font-weight: bold;
+        font-stretch: extra-condensed;
+        font-size: 14px;
+    }
+`;
+
+export const Gamesavailable = styled.div`
+    width: 137.8px;
+    height: 100%;
+    margin-right: 5px;
+
+    & span:nth-child(2) {
+        background-color: indianred;
+        padding-right: 10px;
+        padding-left: 10px;
+        padding-top: 1px;
+        padding-bottom: 1px;
+        font-size: 13px;
+        border-radius: 50px;
+        background-color: #8CA800;
+        margin-left: 5px;
+    }
+`;
+
+export const Gameslive = styled.div`
+    width: 71.8px;
+    height: 100%;    
+    margin-right: 15px;
+
+    & span:nth-child(2) {
+        background-color: indianred;
+        padding-right: 10px;
+        padding-left: 10px;
+        padding-top: 1px;
+        padding-bottom: 1px;
+        font-size: 13px;
+        border-radius: 50px;
+        background-color: #2196FD;
+        margin-left: 5px;
+    }
+`;
+
+export const Gamesvetoing = styled.div`
+    width: 97.3px;
+    height: 100%;
+    
+    & span:nth-child(2) {
+        background-color: indianred;
+        padding-right: 10px;
+        padding-left: 10px;
+        padding-top: 1px;
+        padding-bottom: 1px;
+        font-size: 13px;
+        border-radius: 50px;
+        background-color: #3C3E48;
+        margin-left: 5px;
+    }
+`;
+
+export const Litemode = styled.div`
+    width: 132px;
+    height: 24px;
+    margin-left: 4px;
+    padding-left: 15px;
+    border-left: 1px solid #7d7d82;
+    display: flex;
+    align-items: center;
+    justify-content: start;
+
+    & span {
+        color: white;
+        font-weight: bold;
+        font-size: 15px;
+    }
+`;
+
+export const Litemodecheckcontainer = styled.div`
+    width: 35px;
+    height: 24px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 5px;
+    position: relative;
+    border-radius: 20px;
+`;
+
+export const Litemodecheck = styled.div`
+    width: 15px;
+    height: 15px;
+    background-color: white;
+    border-radius: 50%;
+    position: absolute;
+    left: 10%;
+`;
+
+export const Litemodeinput = styled.input`
+    width: 100%;
+    height: 100%;
+    -webkit-appearance: none;
+    border-radius: 40px;
+    border: 1px solid var(--header-border-color);
+
+    &:checked {
+        background-color: #2196FD;
+    }
 `;
 
 
